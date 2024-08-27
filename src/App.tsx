43bc0d5262b76012a0a9f47e06d0888f6b1ce2b7
miguel <miguel@tonio.dev>
2024-08-27 12:59:02 +0200

    created GameScreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { InsertNameScreen } from "./components/InsertNameScreen";
+//import { InsertNameScreen } from "./components/InsertNameScreen";
+import { GameScreen } from "./components/GameScreen";
 
 const App = () => {
   return (
     <main className="flex items-center justify-center h-screen bg-slate-300">
       <div className="w-3/4 md:w-1/3 p-2 rounded">
-        <InsertNameScreen />
+      {/* <InsertNameScreen /> */}
+       <GameScreen />
       </div>
     </main>
   );
--- src/components/GameScreen/index.tsx
+++ src/components/GameScreen/index.tsx
@@ -1,3 +1,12 @@
 export const GameScreen = () => {
-  return <div></div>;
+  
+  const currentLetter = 'A';
+
+  return (
+    <section className="text-9xl text-center">
+      <div className="text-slate-800"> 
+        <span className="text-slate-400">[</span> {currentLetter} <span className="text-slate-400">]</span> 
+      </div>
+    </section>
+  )
 };
