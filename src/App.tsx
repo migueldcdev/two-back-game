77784e8bb8f5248c1211eade4eb2477a80868351
miguel <miguel@tonio.dev>
2024-08-28 13:21:11 +0200

    reimplementing gameScreen logic & state 0.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import { useReducer } from "react";
-
 import { GameScreen } from "./components/GameScreen";
 import { InsertNameScreen } from "./components/InsertNameScreen";
 import { ResultScreen } from "./components/ResultScreen";
+
 import { gamePhaseReducer, initialGamePhaseState } from "./reducers/gamePhaseReducer";
 
 const App = () => {
-  const [state, dispatch] = useReducer(gamePhaseReducer, initialGamePhaseState);
+  const [gamePhaseState, gamePhaseDispatch] = useReducer(gamePhaseReducer, initialGamePhaseState);
 
   return (
     <main className="flex items-center justify-center h-screen bg-slate-300">
       <div className="w-3/4 md:w-1/3 p-2 rounded">
-        {state.count === 0 && <InsertNameScreen incrementGamePhase={dispatch} />}
-        {state.count === 1 && <GameScreen />}
-        {state.count === 2 && <ResultScreen />}
+        {gamePhaseState.count === 0 && <InsertNameScreen incrementGamePhase={gamePhaseDispatch} />}
+        {gamePhaseState.count === 1 && <GameScreen />}
+        {gamePhaseState.count === 2 && <ResultScreen />}
       </div>
     </main>
   );
