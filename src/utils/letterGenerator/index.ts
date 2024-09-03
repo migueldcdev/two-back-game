e973e09f02c1dd393cf3dc8a200eefcaa9e75f46
miguel <miguel@tonio.dev>
2024-09-03 08:49:25 +0200

    letter generator removed hard coded value

--- src/utils/letterGenerator/index.ts
+++ src/utils/letterGenerator/index.ts
@@ -1,7 +1,7 @@
 export const letters = ["A", "B", "C", "D", "E", "H", "I", "K", "L", "M", "O"];
 
 function generateRandomNumber() {
-  return Math.floor(Math.random() * 11);
+  return Math.floor(Math.random() * letters.length);
 }
 
 export function generateLetter() {
