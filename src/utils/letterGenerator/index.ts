20f9d05753e065458de1543c2b169660d5addd49
miguel <miguel@tonio.dev>
2024-09-04 09:07:31 +0200

    merged util function into one

--- src/utils/letterGenerator/index.ts
+++ src/utils/letterGenerator/index.ts
@@ -1,9 +1,7 @@
 export const letters = ["A", "B", "C", "D", "E", "H", "I", "K", "L", "M", "O"];
 
-function generateRandomNumber() {
-  return Math.floor(Math.random() * letters.length);
-}
-
 export function generateLetter() {
-  return letters[generateRandomNumber()];
+  const randomNumber = Math.floor(Math.random() * letters.length);
+
+  return letters[randomNumber];
 }
