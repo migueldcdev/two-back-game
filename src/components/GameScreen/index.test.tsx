39c421c8c9743689f202fed34b9e1a768a844d39
dev <migueldc.dev@protonmail.com>
2024-09-04 20:09:48 +0200

    added tests for GameScreen component

---
+++ src/components/GameScreen/index.test.tsx
@@ -0,0 +1,44 @@
+import { userEvent } from "@testing-library/user-event";
+import { describe, expect, test, vi } from "vitest";
+import { GameScreen } from ".";
+import { render, screen } from "../../context/GameContext/testGameContext";
+
+const context = {
+  gameState: {
+    userName: "",
+    twoBackLetter: "A",
+    previousLetter: "B",
+    currentLetter: "C",
+    countCycle: 3,
+    showLetter: true,
+    userClickIsWrong: false,
+    userClickIsCorrect: false,
+    gamePhase: 2,
+    correctAnswers: 2,
+    wrongAnswers: 0,
+    analytics: false,
+    notification: "",
+  },
+  gameDispatch: () => {},
+  checkUserClickResult: () => {},
+};
+
+describe("Test suite for GameScreen component", () => {
+  test("should render current letter", () => {
+    render(<GameScreen />, context);
+    const currentLetter = screen.getByText("C");
+    expect(currentLetter).toBeDefined();
+  });
+
+  test("two back button should work", async () => {
+    const user = userEvent;
+    const checkUserClickResult = vi.fn();
+    render(<GameScreen />, { ...context, checkUserClickResult });
+
+    const twoBackButton = screen.getByRole("button");
+
+    await user.click(twoBackButton);
+
+    expect(checkUserClickResult).toBeCalled();
+  });
+});
